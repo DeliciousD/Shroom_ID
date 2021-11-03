aec299ec55293fa392fd92f9718bb84a44151fae
Daniel White <32203653+DeliciousD@users.noreply.github.com>
2021-11-03 17:41:34 +0000

    uploaded manually from file explorer

--- Shrooms/Shrooms_ID_app.py
+++ Shrooms/Shrooms_ID_app.py
@@ -5,7 +5,7 @@ from PIL import Image
 import pandas as pd
 # import tensorflow.keras.backend as K
 
-checkpoint_path = 'Shrooms/checkpoints - Copy/test_save/saved_model.pb'
+checkpoint_path = 'Shrooms/model/my_model/'
 
 @st.cache(allow_output_mutation=True)
 def open_model():
